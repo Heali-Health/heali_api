b2b3e90c594d21626f373fca235196602ab766eb
Bruno Cruz <bru.cruz@gmail.com>
2020-07-22 18:24:55 -0300

    Correction: Ensure authenticated on quote posting

--- src/modules/quotes/infra/http/routes/quotes.routes.ts
+++ src/modules/quotes/infra/http/routes/quotes.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 
 import QuotesController from '@modules/quotes/infra/http/controllers/QuotesController';
+import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 
 const quotesRouter = Router();
 const quotesController = new QuotesController();
 
-quotesRouter.post('/', quotesController.create);
+quotesRouter.post('/', ensureAuthenticated, quotesController.create);
 
 export default quotesRouter;

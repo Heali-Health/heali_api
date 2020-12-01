ca17d3965793f3032a1c30af1c8cf2a040607d73
Bruno Cruz <bru.cruz@gmail.com>
2020-12-01 14:42:02 -0300

    new fix

--- src/modules/payments/services/CreateOrderService.ts
+++ src/modules/payments/services/CreateOrderService.ts
@@ -5,6 +5,12 @@ import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import IMailMarketingProvider from '@shared/container/providers/MailMarketingProvider/models/IMailMarketingProvider';
 import ICreateOrderDTO from '../dtos/ICreateOrderDTO';
 
+interface IError {
+  type: string;
+  title: string;
+  status: number;
+  detail: string;
+}
 @injectable()
 export default class CreateOrderService {
   constructor(
@@ -31,43 +37,50 @@ export default class CreateOrderService {
       product => product.id,
     );
 
-    payment.items.map(async item => {
-      if (!mailMarketingProductsIds.includes(item.id)) {
-        await this.mailMarketingProvider.addProduct({
+    try {
+      payment.items.map(async item => {
+        if (!mailMarketingProductsIds.includes(item.id)) {
+          await this.mailMarketingProvider.addProduct({
+            id: item.id,
+            title: item.title,
+            variants: [
+              {
+                id: item.id,
+                title: item.title,
+              },
+            ],
+          });
+        }
+      });
+
+      await this.mailMarketingProvider.addOrder({
+        id: payment.id.toString(),
+        currency_code: 'BRL',
+        customer: {
+          id: user.id,
+          email_address: user.email,
+          first_name: user.first_name,
+          last_name: user.last_name,
+          opt_in_status: true,
+        },
+        lines: payment.items.map(item => ({
           id: item.id,
-          title: item.title,
-          variants: [
-            {
-              id: item.id,
-              title: item.title,
-            },
-          ],
-        });
-      }
-    });
+          price: item.unit_price,
+          product_id: item.id,
+          product_title: item.title,
+          product_variant_id: item.id,
+          product_variant_title: item.title,
+          quantity: item.quantity,
+        })),
+        order_total: payment.amount,
+      });
 
-    await this.mailMarketingProvider.addOrder({
-      id: payment.id.toString(),
-      currency_code: 'BRL',
-      customer: {
-        id: user.id,
-        email_address: user.email,
-        first_name: user.first_name,
-        last_name: user.last_name,
-        opt_in_status: true,
-      },
-      lines: payment.items.map(item => ({
-        id: item.id,
-        price: item.unit_price,
-        product_id: item.id,
-        product_title: item.title,
-        product_variant_id: item.id,
-        product_variant_title: item.title,
-        quantity: item.quantity,
-      })),
-      order_total: payment.amount,
-    });
-
-    await this.mailMarketingProvider.deleteCart(bagId);
+      await this.mailMarketingProvider.deleteCart(bagId);
+    } catch (err) {
+      throw new AppError(
+        `error type: ${err.type} \n error title: ${err.title} \n error detail: ${err.detail}`,
+        err.status,
+      );
+    }
   }
 }
